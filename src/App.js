f772c9e1201fb06bc6849f43029340e49b185ff1
Daniel <u1135697579@gmail.com>
2026-01-31 17:13:12 +0000

    atualizações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Select(event)
+  {
+    const elementos = event.target.parentElement.querySelectorAll("*")
+    const div = event.target
+    elementos.forEach(elemento => {
+      if(div.tagName == "DIV")
+      {
+        elemento.style.background = "#f3f3f3"
+      }
+    });
+    
+    div.style.background = "#5f7cfd"
+  }
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="side-bar">
+        <h1>TrackMyDeal</h1>
+        <div className="options">
+          <div className = "Produtos" onClick={Select}><p><b>Produtos</b></p></div>
+          <div className = "Configurações" onClick={Select}><p><b>Configurações</b></p></div>
+        </div>
+      </div>
     </div>
   );
 }
